36082b779b2b4b9629c0f3320f71c73e8e534d25
matheus <matheusamenos@gmail.com>
2023-09-07 23:36:26 -0300

    Abulu bebe

--- miniblog/urls.py
+++ miniblog/urls.py
@@ -21,7 +21,22 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('', RedirectView.as_view(url='blog/')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # Alterar quando for pra prod.
+
+
+'''
+    Endpoints provided by the accounts url
+accounts/ login/ [name='login']
+accounts/ logout/ [name='logout']
+accounts/ password_change/ [name='password_change']
+accounts/ password_change/done/ [name='password_change_done']
+accounts/ password_reset/ [name='password_reset']
+accounts/ password_reset/done/ [name='password_reset_done']
+accounts/ reset/<uidb64>/<token>/ [name='password_reset_confirm']
+accounts/ reset/done/ [name='password_reset_complete']
+
+'''
